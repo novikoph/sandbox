33bdf09867321aac84a858a946ae548b69b02cdb
Aleksandr Novikov <novikoph@gmail.com>
2019-12-07 12:42:18 +0300

    FizzBuzz

--- fizzbuzz.py
+++
@@ -1,9 +0,0 @@
-for i in range(1, 101):
-    if i % 15 == 0:
-        print('{0} FizzBuzz'.format(i))
-    elif i % 3 == 0:
-        print('{0} Fizz'.format(i))
-    elif i % 5 == 0:
-        print('{0} Buzz'.format(i))
-    else:
-        print(i)
