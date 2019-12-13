b47c7e6d32148a699ad55bd8ccf990104c951ab7
Aleksandr Novikov <novikoph@gmail.com>
2019-12-13 22:16:26 +0300

    Task solution

---
+++ ProjectEuler/Task3.py
@@ -0,0 +1,14 @@
+def simpleDividers(n):
+    answer = []
+    d = 2
+    while d * d <= n:
+        if n % d == 0:
+            answer.append(d)
+            n //= d
+        else:
+            d += 1
+    if n > 1:
+        answer.append(n)
+    return answer
+
+print(max(simpleDividers(600851475143)))
