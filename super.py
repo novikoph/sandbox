9781b2b3da8013e482a54ed02b9f5d06bf43ac6e
Aleksandr Novikov <novikoph@gmail.com>
2019-12-30 17:46:12 +0300

    Upd

--- super.py
+++ super.py
@@ -4,22 +4,21 @@ home = input('Кто играет дома?: ')
 visitor = input('Кто играет в гостях?: ')
 result = []
 
-for i in range(1020000):
+for i in range(10):
     preres = random.randint(0, 2)
-    # print(preres)
     result.append(preres)
 
-# print(result)
+print(result)
 
 homeWin     = result.count(1)
 visitorWin  = result.count(2)
 draw        = result.count(0)
 
 total = [homeWin, visitorWin, draw]
-# print(total)
+print(total)
 
 max = max(total)
-# print(max)
+print(max)
 
 if homeWin == max:
     print('Победит команда ' + home)
