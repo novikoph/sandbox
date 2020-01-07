c1509790df447e77db0ffe27fd4a63adeb6330f8
Aleksandr Novikov <novikoph@gmail.com>
2020-01-07 18:34:32 +0300

    Task solution

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,9 @@
 for i in range(1, 101):
     if i % 15 == 0:
-        print('{0} FizzBuzz'.format(i))
+        print('{} FizzBuzz'.format(i))
     elif i % 5 == 0:
-        print('{0} Buzz'.format(i))
+        print('{} Buzz'.format(i))
     elif i % 3 == 0:
-        print('{0} Fizz'.format(i))
+        print('{} Fizz'.format(i))
     else:
         print(i)
